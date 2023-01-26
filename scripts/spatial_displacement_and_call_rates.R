baaa211e5dc2705edf66375db5d8c4b74243b9c7
vlad_de <demartev@gmail.com>
2023-01-26 16:55:46 +0100

    260123

--- scripts/spatial_displacement_and_call_rates.R
+++ scripts/spatial_displacement_and_call_rates.R
@@ -1,10 +1,29 @@
 #This is a script for looking at meerkat call rate as a function of spatial displacement.
-#It uses Baptisite's track discritisation data and cross references it with the audio
+#It uses Baptiste's track discretization data and cross references it with the audio
 
-setwd("C:/Users/vdemartsev/My Cloud/Git_projects/CC_dynamics/discreet_tracks_data")
+setwd("C:/Users/vdemartsev/My Cloud/Git_projects/CC_dynamics/")
 
 #set the displacement distance 
 distance <- 5
 
-#load respective data object
-load(paste("spatialMetrics_", distance, "m.RData", sep = ""))
+#load movement data object
+load(paste("discreet_tracks_data.spatialMetrics_", distance, "m.RData", sep = ""))
+
+#load audio data
+call_data <- read.csv("foc_calls_resolved.csv")
+
+#get all dates
+dates <- unique(spatialMetrics$date)
+ 
+for(date in 1:length (dates)) { 
+  date_select <- spatialMetrics[which(spatialMetrics$date == dates[date]), ]
+  individuals <- unique(date_select$indIdx)
+  for (individual in individuals) { 
+    individual_select <- date_select[which(date_select$indIdx == individual) ,]
+    ind_code <- allIndInfo[which(allIndInfo$idx == individual) , "code"]
+    calls_select <- call_data[which(call_data$date == dates[date] & call_data$ind == individual) ,]
+    
+  }
+  
+}
+
