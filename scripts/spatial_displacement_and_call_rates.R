4217f4e7d5fdac7eff6f41db8e675125a902e530
vlad_de <demartev@gmail.com>
2023-01-26 15:54:41 +0100

    new commit

--- scripts/spatial_displacement_and_call_rates.R
+++ scripts/spatial_displacement_and_call_rates.R
@@ -2,3 +2,9 @@
 #It uses Baptisite's track discritisation data and cross references it with the audio
 
 setwd("C:/Users/vdemartsev/My Cloud/Git_projects/CC_dynamics/discreet_tracks_data")
+
+#set the displacement distance 
+distance <- 5
+
+#load respective data object
+load(paste("spatialMetrics_", distance, "m.RData", sep = ""))
