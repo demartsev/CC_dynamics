d0dc34d0b49af4fb4f5ea8f4d4a6d685bca14ff0
vlad_de <demartev@gmail.com>
2023-01-26 15:43:18 +0100

    adding the spatial displacemnt script

---
+++ scripts/spatial_displacement_and_call_rates.R
@@ -0,0 +1,4 @@
+#This is a script for looking at meerkat call rate as a function of spatial displacement.
+#It uses Baptisite's track discritisation data and cross references it with the audio
+
+setwd("C:/Users/vdemartsev/My Cloud/Git_projects/CC_dynamics/discreet_tracks_data")
